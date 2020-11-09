ff4595c245b430a81c20227a0369cf8382dd7e4f
putradev <putraswa@gmail.com>
2020-11-09 13:13:36 +0700

    assignment week 2

---
+++ app/src/main/java/udacoding/test/assignmentudacoding/MainActivity.kt
@@ -0,0 +1,11 @@
+package udacoding.test.assignmentudacoding
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
